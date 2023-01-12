2d5d4084350dd707793a2ddedd144a23ac45ffd4
M1000 <76665071+NormalGuy42@users.noreply.github.com>
2023-01-12 23:17:49 +0000

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -81,3 +81,20 @@ function add(){
 //   console.log('home was added')
 // }
 
+//Viewproperty code
+const buttons = document.querySelectorAll("[data-slide-btn]");
+buttons.forEach(button =>{
+  button.addEventListener("click",()=>{
+    const offset = button.dataset.slideBtn === "next" ?1:-1
+    const slides = button.closest("[data-slide-container]").querySelector("[data-slides]")
+    const activeSlide = slides.querySelector("[data-active]")
+
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if(newIndex < 0) newIndex = slides.children.length -1
+    if(newIndex >= slides.children.length) newIndex = 0
+    
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
+
