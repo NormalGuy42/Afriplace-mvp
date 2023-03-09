e077b4e66e5f099433d218f19e62fb0fb494f92d
Madiou <diabatluffy@gmail.com>
2023-03-09 21:40:39 +0000

    New commit

--- user/user_script.js
+++ user/user_script.js
@@ -3,3 +3,12 @@ navBtn.addEventListener('click',()=>{
     const navMenu = document.querySelector('.sidebar_nav');
     navMenu.classList.toggle('showSideBarNav');
 })
+
+document.addEventListener('click',(e)=>{
+    if(!e.target.closest('.burger')){
+        const navMenu = document.querySelector('.sidebar_nav');
+        if(!e.target.closest('.sidebar_nav') && navMenu.classList.contains('showSideBarNav')){
+            navMenu.classList.remove('showSideBarNav');
+        }
+    }
+})
